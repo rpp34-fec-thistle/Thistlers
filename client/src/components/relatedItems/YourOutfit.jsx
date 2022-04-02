4b295aa72c79d0dde68830aaa3483d01ba804977
Jane Srisarakorn <janedoenut@gmail.com>
2022-04-01 22:07:02 -0700

    Created relatedItems folder with components in for client/src folder

---
+++ client/src/components/relatedItems/YourOutfit.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Carousels from './Carousels.jsx';
+
+class YourOutfit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <h1>This will render the Your Outfit carousel.</h1>
+    )
+  }
+}
+
+export default YourOutfit;

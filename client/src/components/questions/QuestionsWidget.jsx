a81d9be829db7e68ff34c90be9f93862252b5ffe
Derek Southard <derekmsouthard@gmail.com>
2022-04-04 13:12:26 -0700

    Added server route to GET all questions for a given product ID. Added componentDidMount method in top level Question component to call the server and set the question data in state.

--- client/src/components/questions/QuestionsWidget.jsx
+++ client/src/components/questions/QuestionsWidget.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Search from './Search.jsx';
 import QuestionList from './QuestionList.jsx';
 import Footer from './Footer.jsx';
+import axios from 'axios';
 
 class QuestionWidget extends Component {
   constructor(props) {
@@ -11,6 +12,17 @@ class QuestionWidget extends Component {
       displayedQuestions: []
     }
   }
+
+  componentDidMount() {
+    axios('/questions/64621')
+    .then(results => {
+      this.setState({ questions: results.data.results })
+    })
+    .catch(err => {
+      console.error('err: ', err);
+    })
+  }
+
   render() {
     return (
       <div className="question-widget">

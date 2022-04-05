a296b135136f309432d0e81f23f7bfe3ff1614fc
Derek Southard <derekmsouthard@gmail.com>
2022-04-04 18:31:38 -0700

    Replaced depricated ReactDOM.render method from index.js.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
 
-
-ReactDOM.render(<App/> , document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App/>);

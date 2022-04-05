0fa5c8a6fc51cc2e128b722278359572af23258a
Darian Hogue <hogued96@gmail.com>
2022-04-05 11:29:51 -0400

    Added Thumbnails and selected functionality

--- client/src/components/overview/image-gallery/default-view-components/ImageThumbnail.jsx
+++ client/src/components/overview/image-gallery/default-view-components/ImageThumbnail.jsx
@@ -1,20 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ImageThumbnail extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = {}
 
-    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.props.selectedPhoto(+e.target.id)
   }
 
   render() {
+    let pageElement;
+    if (this.props.currentImage === this.props.imageId) {
+      pageElement = (
+          <img onClick={this.handleClick}
+          id={this.props.image_index}
+          src={this.props.thumbnail}
+          className="image-tn-selected"
+          ></img>
+      )
+    } else {
+      pageElement = (
+        <img
+        src={this.props.thumbnail}
+        className="image-tn"
+        onClick={this.handleClick}
+        id={this.props.image_index}
+        ></img>
+      )
+    }
     return (
-      <p className="image-tn">
-        ImageThumbnial [Placeholder]
-      </p>
+      pageElement
     )
   }
+}
+
+ImageThumbnail.propTypes = {
+  thumbnail: PropTypes.string,
+  currentImage: PropTypes.string,
+  imageId: PropTypes.string,
+  selectedPhoto: PropTypes.func,
+  image_index: PropTypes.number
 }
 
 export default ImageThumbnail;

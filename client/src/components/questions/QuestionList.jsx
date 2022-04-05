0e99f9319dc1911f2124164c201f03668e050656
Derek Southard <derekmsouthard@gmail.com>
2022-04-05 12:38:45 -0700

    Added functionality to display only top two answers for each question. When the 'show more answers' button is clicked the remaining answers are displayed and the button disappears.

--- client/src/components/questions/QuestionList.jsx
+++ client/src/components/questions/QuestionList.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import Question from './Question.jsx';
 import PropTypes from 'prop-types';
 
-const QuestionList = ({ questions }) => {
+const QuestionList = ({ questions, onShowMoreAnswersClick }) => {
   return (
     <div className="question-list">
       {questions.map(question => {
-        return <Question question={question} key={question.question_id}/>;
+        return <Question question={question} key={question.question_id} onShowMoreAnswersClick={onShowMoreAnswersClick}/>;
       })}
     </div>
   )
 };
 
 QuestionList.propTypes = {
-  questions: PropTypes.array.isRequired
+  questions: PropTypes.array.isRequired,
+  onShowMoreAnswersClick: PropTypes.func.isRequired
 };
 
 export default QuestionList;

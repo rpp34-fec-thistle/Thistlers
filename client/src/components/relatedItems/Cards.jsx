4f35e05ab8d563a58f7ebaf64b708c852a644035
Jane Srisarakorn <janedoenut@gmail.com>
2022-04-09 12:52:23 -0700

    In progress

--- client/src/components/relatedItems/Cards.jsx
+++ client/src/components/relatedItems/Cards.jsx
@@ -1,88 +1,87 @@
-import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-import axios from 'axios';
-import Ratings from './Ratings.jsx';
+// import React, { Component } from 'react';
+// // import PropTypes from 'prop-types';
+// import axios from 'axios';
+// import Ratings from './Ratings.jsx';
 
-class Cards extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cardType: '',
-      id: 0,
-      image: '',
-      price: 0,
-      category: '',
-      name: '',
-      reviews: ''
-    }
-    this.setCard = this.setCard.bind(this);
-    this.setDescription = this.setDescription.bind(this);
-  }
+// class Cards extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       id: 0,
+//       image: '',
+//       price: 0,
+//       category: '',
+//       name: '',
+//       reviews: ''
+//     }
+//     this.setCard = this.setCard.bind(this);
+//     this.setDescription = this.setDescription.bind(this);
+//   }
 
-  setCard() {
-    axios(`http://localhost:8080/styles/${this.state.id}`)
-    .then((data) => {
-      var result = data.data;
-      // console.log(result);
-      this.setState({
-        id: result.product_id,
-        image: result.results[0].photos[0].thumbnail_url,
-        price: result.results[0].original_price
-      });
-      return result;
-    })
-    .then(() => {
-      this.setDescription();
-    })
-    .catch((err) => {
-      // console.log('setCard error');
-      return err;
-    })
+//   setCard() {
+//     axios(`http://localhost:8080/styles/${this.state.id}`)
+//     .then((data) => {
+//       var result = data.data;
+//       // console.log(result);
+//       this.setState({
+//         id: result.product_id,
+//         image: result.results[0].photos[0].thumbnail_url,
+//         price: result.results[0].original_price
+//       });
+//       return result;
+//     })
+//     .then(() => {
+//       this.setDescription();
+//     })
+//     .catch((err) => {
+//       // console.log('setCard error');
+//       return err;
+//     })
 
-  }
+//   }
 
-  setDescription() {
-    axios(`http://localhost:8080/products/${this.state.id}`)
-    .then((data) => {
-      var result = data.data;
-      // console.log(result);
-      this.setState({
-        category: result.category,
-        name: result.name
-      });
-      return result;
-    })
-    .catch((err) => {
-      console.log('setDescription error');
-      return err;
-    })
-  }
+//   setDescription() {
+//     axios(`http://localhost:8080/products/${this.state.id}`)
+//     .then((data) => {
+//       var result = data.data;
+//       // console.log(result);
+//       this.setState({
+//         category: result.category,
+//         name: result.name
+//       });
+//       return result;
+//     })
+//     .catch((err) => {
+//       console.log('setDescription error');
+//       return err;
+//     })
+//   }
 
-  componentDidMount() {
-    this.setCard();
-  }
+//   componentDidMount() {
+//     this.setCard();
+//   }
 
-  render() {
+//   render() {
 
-    return(
-      <div className="card" key={this.state.id}>
-        <div className="card-image">
-        <img src={this.state.image} />
-        </div>
-        <div className="card-description">
-          {this.state.category}
-          {this.state.name}
-          {this.state.price}
-          <Ratings />
-        </div>
-      </div>
-    )
-  }
-}
-
-// Cards.propTypes = {
-//   setCard: PropTypes.func.isRequired,
-//   setDescription: PropTypes.func.isRequired
+//     return(
+//       <div className="card" key={this.state.id}>
+//         <div className="card-image">
+//         <img src={this.state.image} />
+//         </div>
+//         <div className="card-description">
+//           {this.state.category}
+//           {this.state.name}
+//           {this.state.price}
+//           <Ratings />
+//         </div>
+//       </div>
+//     )
+//   }
 // }
 
-export default Cards;
+// // Cards.propTypes = {
+// //   setCard: PropTypes.func.isRequired,
+// //   setDescription: PropTypes.func.isRequired
+// // }
+
+// export default Cards;
--- client/src/components/relatedItems/RelatedProducts.jsx
+++ client/src/components/relatedItems/RelatedProducts.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Cards from './Cards.jsx';
 import axios from 'axios';
+import Ratings from './Ratings.jsx';
 
 class RelatedProducts extends Component {
   constructor(props) {
@@ -9,9 +9,38 @@ class RelatedProducts extends Component {
       testOverviewId: 64620,
       relatedProductsIds: [],
       relatedProductsData: [],
-      currentCard: 0,
+      // id: 0,
+      // image: '',
+      // price: 0,
+      // category: '',
+      // name: '',
+      // reviews: ''
     };
     this.setRelatedProductsId = this.setRelatedProductsId.bind(this);
+    this.setCard = this.setCard.bind(this);
+    this.setDescription = this.setDescription.bind(this);
+    this.setRelatedProductsData = this.setRelatedProductsData.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({loading: true});
+
+    Promise.all([
+      this.setRelatedProductsId(),
+      this.setRelatedProductsData()
+    ]).then(values => {
+        return values[0].concat(values[1]);
+    }).then(results => {
+        this.setState({
+            articles: this.state.articles.concat(results),
+            loading: "none"
+        });
+    }).catch(err => {
+        console.log('Oops, something went wrong', err);
+    });
+
+
+
   }
 
   setRelatedProductsId() {
@@ -27,14 +56,70 @@ class RelatedProducts extends Component {
         console.log('error in setRelatedProducts');
         return err;
       })
-
-  //   // take currentId
-  //   // look up relatedItems via axios request to server
-  //   // write express API query / GET request via relatedItems:id
   }
 
-  componentDidMount() {
-    this.setRelatedProductsId();
+  setRelatedProductsData() {
+    for (var id in this.relatedProductsIds) {
+      this.setState({
+        id: id
+      });
+      this.setCard();
+      var newObj = {
+        id: this.state.id,
+        image: this.state.image,
+        price: this.state.price,
+        category: this.state.category,
+        name: this.state.name,
+        reviews: this.state.reviews
+      };
+      var newArr = this.state.relatedProductsData.push(newObj);
+      this.setState({
+        relatedProductsData: newArr
+      })
+    }
+  }
+
+  setCard() {
+    axios(`http://localhost:8080/styles/${this.state.id}`)
+    .then((data) => {
+      var result = data.data;
+      var newObj = {
+        id: result.product_id,
+        image: result.results[0].photos[0].thumbnail_url,
+        price: result.results[0].original_price
+      };
+      console.log('setCard obj: ', newObj);
+      return newObj;
+    })
+    .then(() => {
+      this.setDescription();
+    })
+    .catch((err) => {
+      console.log('setCard error');
+      return err;
+    })
+
+  }
+
+  setDescription(obj) {
+    axios(`http://localhost:8080/products/${this.state.id}`)
+    .then((data) => {
+      var result = data.data;
+      var newObj2 = {
+        category: result.category,
+        name: result.name
+      };
+      var allData = Object.assign(obj, newObj2);
+      var updateData = this.state.relatedProductsData.push(allData);
+      this.setState({
+        relatedProductsData: updateData
+      });
+      return result;
+    })
+    .catch((err) => {
+      console.log('setDescription error');
+      return err;
+    })
   }
 
   // to render, take this.state.relatedProductsId and for each item, render a card using its ID
@@ -42,8 +127,16 @@ class RelatedProducts extends Component {
   render() {
 
     return(
-      <div className="relatedProducts">
-        <Cards /><Cards /><Cards /><Cards />
+      <div className="related-item-card" key={this.state.id}>
+        <div className="related-item-card-image">
+        <img src={this.state.image} />
+        </div>
+        <div className="related-item-card-description">
+          {this.state.category}
+          {this.state.name}
+          {this.state.price}
+          <Ratings />
+        </div>
       </div>
     )
   }

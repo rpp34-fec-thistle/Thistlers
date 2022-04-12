39112945b5a0ae89c749fa657d73341a880185bf
Darian Hogue <hogued96@gmail.com>
2022-04-12 14:16:17 -0400

    Added BMR functionality to styleSelector

--- client/src/components/overview/style-selector/StyleSelector.jsx
+++ client/src/components/overview/style-selector/StyleSelector.jsx
@@ -5,20 +5,18 @@ import SelectedStyle from './components/SelectedStyle.jsx';
 import AllStyleThumbnails from './components/AllStyleThumbnails.jsx';
 
 function StyleSelector(props) {
-  let updateStyle = (styleIndex) => {
-    console.log({styleIndex})
-  }
     return(
       <div className="style-selector">
         <SelectedStyle styleName={props.styles[props.styleIndex]?.name}/>
-        <AllStyleThumbnails updateStyle={updateStyle} data={props.styles[props.styleIndex]}/>
+        <AllStyleThumbnails currentStyle={props.styleIndex} updateStyle={props.updateStyle} data={props.styles}/>
       </div>
     )
 }
 
 StyleSelector.propTypes = {
   styles: PropTypes.array,
-  styleIndex: PropTypes.number
+  styleIndex: PropTypes.number,
+  updateStyle: PropTypes.func
 }
 
 export default StyleSelector;

bcbbf5b9896afdb9c39e9e23c3d735a895d22cd2
Derek Southard <derekmsouthard@gmail.com>
2022-04-14 17:48:56 -0700

    update Add Question modal css

--- client/src/components/questions/Footer.jsx
+++ client/src/components/questions/Footer.jsx
@@ -52,46 +52,59 @@ class Footer extends Component {
           <form className="add-question-form">
             <div className="close-modal-container">
               <p> </p>
-              <p onClick={this.onFormClose} className="close-modal">x</p>
+              <div className="close-modal-contents">
+                <p onClick={this.onFormClose} className="close-modal">X</p>
+              </div>
             </div>
             <p className="add-question-title">Ask Your Question</p>
             <p className="add-question-subtitle">About the product <span className="product-name">{this.props.productName}</span></p>
             <div className="nickname-input form-input">
               <div>
-                <label htmlFor="nickname">nickname</label>
+                <label htmlFor="nickname">Nickname *</label>
               </div>
-              <input
-                type="text"
-                id="nickname"
-                name="nickname"
-                onChange={this.onChangeInput}
-                placeholder="Example: jackson11!"
-                >
-                </input>
+              <div className="input-container">
+                <input
+                  type="text"
+                  id="nickname"
+                  name="nickname"
+                  onChange={this.onChangeInput}
+                  placeholder="Example: jackson11!"
+                  maxLength="60"
+                  >
+                  </input>
+              </div>
             </div>
             <div className="nickname-input form-input">
               <div>
-                <label htmlFor="email">email</label>
+                <label htmlFor="email">Email *</label>
               </div>
-              <input
-                type="text"
-                id="email"
-                name="email"
-                onChange={this.onChangeInput}
-                placeholder="sample@email.com"
-                >
+              <div className="input-container">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  onChange={this.onChangeInput}
+                  placeholder="sample@email.com"
+                  maxLength="60"
+                  >
                 </input>
+              </div>
             </div>
             <div className="question-input form-input">
-              <label htmlFor="question"></label>
-              <textarea
-                type="text"
-                id="question"
-                name="question"
-                onChange={this.onChangeInput}
-                placeholder="What would you like to know about this product?"
-              >
-              </textarea>
+              <div>
+                <label htmlFor="question">Question *</label>
+              </div>
+              <div className="input-container">
+                <textarea
+                  type="text"
+                  id="question"
+                  name="question"
+                  onChange={this.onChangeInput}
+                  placeholder="What would you like to know about this product?"
+                  maxLength="60"
+                >
+                </textarea>
+              </div>
             </div>
             <button className="submit-question-button" onClick={this.onQuestionSubmit}>Submit</button>
           </form>

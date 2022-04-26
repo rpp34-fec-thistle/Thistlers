973fbffefd319df8f92a83b6bb50b92c49586c85
Jane Srisarakorn <janedoenut@gmail.com>
2022-04-26 14:00:52 -0700

    Fixed MyOutfit implementation- set to handle new users and persist data

--- client/src/components/relatedItems/YourOutfit.jsx
+++ client/src/components/relatedItems/YourOutfit.jsx
@@ -12,6 +12,7 @@ class YourOutfit extends Component {
     this.setYourOutfits = this.setYourOutfits.bind(this);
     this.getYourOutfits = this.getYourOutfits.bind(this);
     this.deleteYourOutfits = this.deleteYourOutfits.bind(this);
+    this.addToOutfits = this.addToOutfits.bind(this);
   }
 
   componentDidMount() {
@@ -19,13 +20,15 @@ class YourOutfit extends Component {
   }
 
   setYourOutfits() {
-    window.localStorage.clear();
-    window.localStorage.setItem('yourOutfits', [64624, 64628, 64626, 64621, 64620]);
-    let testArray = window.localStorage.getItem('yourOutfits');
-    testArray = testArray.split(',').map(x => parseInt(x));
-    this.setState({
-      yourOutfitIds: testArray
-    })
+    if (window.localStorage.yourOutfits) {
+      let testArray = window.localStorage.getItem('yourOutfits');
+      testArray = testArray.split(',').map(x => parseInt(x));
+      this.setState({
+        yourOutfitIds: testArray
+      })
+    } else {
+      window.localStorage.setItem('yourOutfits', []);
+    }
   }
 
   getYourOutfits(){
@@ -33,17 +36,42 @@ class YourOutfit extends Component {
   }
 
   deleteYourOutfits(id) {
-    let originalArray = this.state.yourOutfitIds;
-    let newArray = [];
-    for (var i = 0; i < originalArray.length; i++) {
-      if (originalArray[i] !== id) {
-        newArray.push(originalArray[i]);
+    if (this.state.yourOutfitIds.length === 1) {
+      window.localStorage.clear();
+      window.localStorage.setItem('yourOutfits', []);
+      this.setState({
+        yourOutfitIds: []
+      })
+    } else {
+      let originalArray = this.state.yourOutfitIds;
+      let newArray = [];
+      for (var i = 0; i < originalArray.length; i++) {
+        if (originalArray[i] !== id) {
+          newArray.push(originalArray[i]);
+        }
       }
+      this.setState({
+        yourOutfitIds: newArray
+      })
+      window.localStorage.setItem('yourOutfits', this.state.yourOutfitIds);
     }
-    this.setState({
-      yourOutfitIds: newArray
-    })
-    window.localStorage.setItem('yourOutfits', this.state.yourOutfitIds);
+  }
+
+  addToOutfits(id) {
+    let originalArray = this.state.yourOutfitIds;
+    if (window.localStorage.yourOutfits === '') {
+      let newArray = [id]
+      this.setState({
+        yourOutfitIds: newArray
+      })
+      window.localStorage.setItem('yourOutfits', [id]);
+    } else {
+      let newArray = [...new Set([id, ...originalArray])];
+      this.setState({
+        yourOutfitIds: newArray
+      })
+      window.localStorage.setItem('yourOutfits', this.state.yourOutfitIds);
+    }
   }
 
   render() {
@@ -56,8 +84,8 @@ class YourOutfit extends Component {
           <h3>Your Outfit</h3>
           <div className="your-outfit-carousel" data-testid="your-outfit-id">
 
-
           <div className="card" data-testid='test-id' id={this.props.id}>
+
 
             <button onClick={() => this.addToOutfits(this.props.overviewId)} className="add-to-outfits">Add This Item to Your Outfit List</button>
 

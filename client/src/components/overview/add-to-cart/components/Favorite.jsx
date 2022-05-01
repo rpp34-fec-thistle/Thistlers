cc62bc9c689b4f3c793817a45a4591d31e0d91bd
Darian Hogue <hogued96@gmail.com>
2022-05-01 15:34:34 -0400

    uncommented out everyone elses components.

---
+++ client/src/components/overview/add-to-cart/components/Favorite.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Favorite extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={}
+  }
+
+  render() {
+    return(
+      <div className="favorite">
+       Favorite [Placeholder]
+      </div>
+    )
+  }
+}
+
+export default Favorite;

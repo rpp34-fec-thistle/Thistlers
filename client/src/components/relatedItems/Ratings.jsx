d8d7a5be871c0a453bedf50127e01e67934f30a5
Jane Srisarakorn <janedoenut@gmail.com>
2022-05-04 20:39:35 -0700

    Removed more commented out code

--- client/src/components/relatedItems/Ratings.jsx
+++ client/src/components/relatedItems/Ratings.jsx
@@ -24,9 +24,9 @@ const Ratings = ({ id, ratings }) => {
   }
 
   return (
-      <div className="ratings" data-testid='ratings-id' id={'rating-' + id}>
-        {starString}
-      </div>
+    <div className="ratings" data-testid='ratings-id' id={'rating-' + id}>
+      {starString}
+    </div>
   )
 };
 
@@ -38,78 +38,14 @@ Ratings.propTypes = {
 export default Ratings;
 
 
+// var result = this.props.ratings;
+// var numWholeStars = Math.floor(result);
+// var remainderStars = averageScore - numWholeStars;
 
+// var addWholeStar = remainderStars > .875;
+// var addThreeQuarterStar = remainderStars < .875 && remainderStars > .625;
+// var addHalfStar = remainderStars < .625 && remainderStars > .37;
+// var addQuarterStar = remainderStars < .37 && remainderStars > .125;
+// var addNoStar = remainderStars < .125;
 
-     // var remainderStars = averageScore - numWholeStars;
-
-          // var addWholeStar = remainderStars > .875;
-          // var addThreeQuarterStar = remainderStars < .875 && remainderStars > .625;
-          // var addHalfStar = remainderStars < .625 && remainderStars > .37;
-          // var addQuarterStar = remainderStars < .37 && remainderStars > .125;
-          // var addNoStar = remainderStars < .125;
-
-      // console.log('rating: ', numWholeStars, addWholeStar, addThreeQuarterStar, addHalfStar, addQuarterStar, addNoStar);
-
-
-
-
-// import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-
-// class Ratings extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-
-//     var result = this.props.ratings;
-//     var numWholeStars = Math.floor(result);
-//     var starString = '';
-
-//     if (numWholeStars === 1) {
-//       starString = '★☆☆☆☆'
-//     }
-//     if (numWholeStars === 2) {
-//       starString = '★★☆☆☆'
-//     }
-//     if (numWholeStars === 3) {
-//       starString = '★★★☆☆'
-//     }
-//     if (numWholeStars === 4) {
-//       starString = '★★★★☆'
-//     }
-//     if (numWholeStars === 5) {
-//       starString = '★★★★★'
-//     }
-
-//     return (
-//       <>
-//         <div className="ratings" data-testid='ratings-id' id={'rating-' + this.props.id}>
-//           {starString}
-//         </div>
-//       </>
-//     )
-
-//   }
-// }
-
-// Ratings.propTypes = {
-//   id: PropTypes.number,
-//   ratings: PropTypes.number
-// }
-
-// export default Ratings;
-
-
-
-
-//      // var remainderStars = averageScore - numWholeStars;
-
-//           // var addWholeStar = remainderStars > .875;
-//           // var addThreeQuarterStar = remainderStars < .875 && remainderStars > .625;
-//           // var addHalfStar = remainderStars < .625 && remainderStars > .37;
-//           // var addQuarterStar = remainderStars < .37 && remainderStars > .125;
-//           // var addNoStar = remainderStars < .125;
-
-//       // console.log('rating: ', numWholeStars, addWholeStar, addThreeQuarterStar, addHalfStar, addQuarterStar, addNoStar);
+// console.log('rating: ', numWholeStars, addWholeStar, addThreeQuarterStar, addHalfStar, addQuarterStar, addNoStar)
